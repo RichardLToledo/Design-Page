2d36b5c89013fa23aa5e9ac728ecd8bfe2f5fa71
Richard <dragonzira@gmail.com>
2020-08-10 15:38:13 -0300

    Linkando estilo do Tema

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wizzer Design</title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"> 
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400;700&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
