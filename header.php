46bf84a33e067f37d4aeb367fb398e8ade4a7e00
Richard <dragonzira@gmail.com>
2020-08-10 15:21:26 -0300

    Subindo arquivos

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wizzer Design</title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400;700&display=swap" rel="stylesheet">
+    <?php wp_head(); ?>
+</head>
+<body>
+<header>
+Cabeçalho
+</header>
